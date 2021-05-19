082220c03d8706877a9a351c63a504b2d485c839
pacciulli <meyer.caue@gmail.com>
2021-05-19 15:36:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Titanic Machine Learn challenge from Kaggle using scikit-learn.
+
+Details: https://www.kaggle.com/c/titanic
---
+++ titanic.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
+
+
+class TitanicPredict(object):
+    def __init__(self):
+        pass
+
+    def load_data(self):
+        pass
+
+    def clean_data(self):
+        pass
+
+    def generate_model(self):
+        pass
+
+    def evaluate_model(self):
+        pass
+
+    def save_response(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
