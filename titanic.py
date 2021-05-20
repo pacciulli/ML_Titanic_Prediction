80d7ccd31fd76db557a7df6d6e06d33fab975ff5
pacciulli <meyer.caue@gmail.com>
2021-05-20 12:05:12 +0900

    Kaggle first submit

--- titanic.py
+++ titanic.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import SGDClassifier, LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 import re
 
 
@@ -11,14 +14,15 @@ if __name__ == "__main__":
 
     formated_data = data.drop(["PassengerId", "Ticket"], axis=1)
 
-    # formated_data.dropna(subset=["Age"], inplace=True)
+    # formated_data.dropna(subset=['Age'], inplace=True)
     age_mean = formated_data["Age"].mean()
     formated_data["Age"].fillna(age_mean, inplace=True)
 
     max_gate = formated_data["Embarked"].value_counts().keys()[0]
     formated_data["Embarked"].fillna(max_gate, inplace=True)
 
-    formated_data["Title"] = formated_data["Name"]
+    formated_data["Title"] = np.nan
+
     for i in range(len(formated_data)):
         formated_data["Title"][i] = re.findall(
             r",\s(\D+\.)\s", formated_data["Name"][i]
@@ -26,11 +30,12 @@ if __name__ == "__main__":
 
     formated_data["Cabin"].fillna("None", inplace=True)
     formated_data["B_Cabins"] = formated_data["Cabin"] != "None"
-    # formated_data["B_Par"] = formated_data["Parch"] > 0
+    # formated_data['B_SibSp'] = formated_data['SibSp'] > 0
+    # formated_data['B_Par'] = formated_data['Parch'] > 0
 
     x = pd.get_dummies(
-        formated_data.drop(["Survived", "Cabin", "Name"], axis=1),
-        columns=["Pclass", "Sex", "Embarked", "Title", "B_Cabins"],
+        formated_data.drop(["Survived", "Cabin", "Name", "Title"], axis=1),
+        columns=["Pclass", "Sex", "Embarked", "B_Cabins"],
     )
     y = formated_data["Survived"]
 
@@ -48,5 +53,42 @@ if __name__ == "__main__":
     titanic_LogReg.fit(x_train, y_train)
     acc_LogReg = titanic_LogReg.score(x_test, y_test)
 
+    titanic_dectree = DecisionTreeClassifier(max_depth=20)
+    titanic_dectree.fit(x_train, y_train)
+    acc_dectree = titanic_dectree.score(x_test, y_test)
+
+    titanic_RandomForest = RandomForestClassifier(max_depth=8)
+    titanic_RandomForest.fit(x_train, y_train)
+    acc_RandomForest = titanic_RandomForest.score(x_test, y_test)
+
     print(f"SGD Classifier: {acc_SGD}")
     print(f"Logistic Regression: {acc_LogReg}")
+    print(f"Decision Tree: {acc_dectree}")
+    print(f"Random Forest: {acc_RandomForest}")
+
+    test_data = pd.read_csv(
+        "https://github.com/pacciulli/ML_Titanic_Prediction/raw/main/data/test.csv"
+    )
+    test_data["Age"].fillna(age_mean, inplace=True)
+    test_data["Embarked"].fillna(max_gate, inplace=True)
+    test_data["Title"] = np.nan
+
+    for i in range(len(test_data)):
+        test_data["Title"][i] = re.findall(r",\s(\D+\.)\s", test_data["Name"][i])[0]
+
+    test_data["Cabin"].fillna("None", inplace=True)
+    test_data["B_Cabins"] = formated_data["Cabin"] != "None"
+    test_data["Fare"].fillna("0", inplace=True)
+
+    test_x = pd.get_dummies(
+        test_data.drop(["Cabin", "Name", "Ticket", "PassengerId", "Title"], axis=1),
+        columns=["Pclass", "Sex", "Embarked", "B_Cabins"],
+    )
+
+    test_data["Survived"] = titanic_LogReg.predict(test_x)
+
+    file = open("titanic_1.csv", "w")
+    file.writelines(f"PassengerId,Survived\n")
+    for i in range(len(test_data)):
+        file.writelines(f"{test_data['PassengerId'][i]},{test_data['Survived'][i]}\n")
+    file.close()
